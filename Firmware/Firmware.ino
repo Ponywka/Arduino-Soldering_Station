f02a62deb2958bd0af67af171e58e0b909a6c219
Ponywka <vladimir.lsk.cool@gmail.com>
2019-12-28 15:45:35 +0300

    Text formating with multiple lines

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -10,30 +10,46 @@
 
 #include "libraries/fontController.h"
 
-#define displayWidth  128 // Ширина экрана в пикселях
-#define displayHeight 64  // Высота экрана в пикселях
+#define displayWidth 128 // Ширина экрана в пикселях
+#define displayHeight 64 // Высота экрана в пикселях
 
 Adafruit_SSD1306 display(displayWidth, displayHeight, &Wire, 4); // Библиотека дисплея
 
-void drawCopyright(){
+int n = 0;
+
+void drawCopyright()
+{
 	display.clearDisplay();
 	//display.drawBitmap(48, 26, image_github, 16, 16, 1);
 	display.drawBitmap(0, 6, image_pony, 32, 53, 1);
-	FontManager::drawTextFormated(display, 32, 0, displayWidth, displayHeight, font_terminus12, (char*)"  Ponywka");
+	FontManager::drawTextFormated(display, 32, 0, displayWidth, displayHeight, font_terminus12, (char *)"GitHub:\n@Ponywka", 1);
 	display.display();
 }
 
-void setup() {
+void setup()
+{
 	Serial.begin(9600);
 	// Инициализация дисплея
-	if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // тут под ставить свой I2C адресс
+	if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3D))
+	{ // тут под ставить свой I2C адресс
 		Serial.println(F("SSD1306 allocation failed"));
-		for(;;);
+		for (;;)
+			;
 	}
 
 	drawCopyright();
+	delay(3000);
 }
 
-void loop() {
+String output = "                ";
 
+void loop()
+{
+	n++;
+	display.clearDisplay();
+	output = String(n);
+	FontManager::drawTextFormated(display, 0, 0, displayWidth, displayHeight, font_terminus24, (char *)output.c_str(), 1);
+	display.display();
+	Serial.print("Frame: ");
+	Serial.println(n);
 }
--- Firmware/libraries/fontController.h
+++ Firmware/libraries/fontController.h
@@ -14,46 +14,99 @@
     ..-..| Массив несжатой картинки (длина до конца)
 */
 
-class FontManager {
-    public:
-        static void drawText(Adafruit_SSD1306 display, int x, int y, const uint8_t *font, char *letters, int color = 1){
-            // Вычисление размера изображения в символе [в байтах]
-            int oneCharSize = pgm_read_byte(font + 1) * pgm_read_byte(font + 2);
-            uint8_t character[oneCharSize]; // Массив с изображением символа
+class FontManager
+{
+public:
+    static void drawText(Adafruit_SSD1306 display, int x, int y, const uint8_t *font, char *letters, int color = 1)
+    {
+        // Вычисление размера изображения в символе [в байтах]
+        int oneCharSize = pgm_read_byte(font + 1) * pgm_read_byte(font + 2);
+        uint8_t character[oneCharSize]; // Массив с изображением символа
 
-            // Поиск символов и вывод их на дисплей
-            int charPos = 0;
-            for(int letterID=0; letterID < strlen(letters); letterID++){
-                // Новое положение пикселя на дисплее (нужно для корректного отображения)
-                int newX = x + pgm_read_byte(font) * letterID;
-                // Перебор всех возможных символов в шрифте
-                for(int i = 4; i < (4 + pgm_read_byte(font + 3)); i++){
-                    if((int)letters[letterID] == pgm_read_byte(font + i)){
-                        charPos = i-4;
-                        break;
-                    } 
-                }
-                // Установка необходимого региона откуда нужно брать изображение символа
-                int offset = 4 + pgm_read_byte(font + 3) + (oneCharSize*charPos);
-                for(int i = offset; i < (offset + oneCharSize); i++){
-                    character[i - offset] = pgm_read_byte(font + i);
-                }
+        // Поиск символов и вывод их на дисплей
+        int charPos = 0;
 
-                // Вывод символа на экран
-                display.drawBitmap(newX, y, character, pgm_read_byte(font + 0), pgm_read_byte(font + 1), color);
+        int letterInLine = 0;
+        int line = 0;
+
+        for (int letterID = 0; letterID < strlen(letters); letterID++)
+        {
+            if ((int)letters[letterID] == '\n')
+            {
+                line++;
+                letterInLine = 0;
+                continue;
+            }
+            // Новое положение пикселя на дисплее (нужно для корректного отображения)
+            int newX = x + pgm_read_byte(font) * letterInLine;
+            int newY = y + pgm_read_byte(font + 1) * line;
+            // Перебор всех возможных символов в шрифте
+            for (int i = 4; i < (4 + pgm_read_byte(font + 3)); i++)
+            {
+                if ((int)letters[letterID] == pgm_read_byte(font + i))
+                {
+                    charPos = i - 4;
+                    break;
+                }
+            }
+            // Установка необходимого региона откуда нужно брать изображение символа
+            int offset = 4 + pgm_read_byte(font + 3) + (oneCharSize * charPos);
+            for (int i = offset; i < (offset + oneCharSize); i++)
+            {
+                character[i - offset] = pgm_read_byte(font + i);
+            }
+
+            // Вывод символа на экран
+            display.drawBitmap(newX, newY, character, pgm_read_byte(font + 0), pgm_read_byte(font + 1), color);
+
+            letterInLine++;
+        }
+    }
+
+    static void drawTextFormated(Adafruit_SSD1306 display, int x1, int y1, int x2, int y2, const uint8_t *font, char *letters, int type = 0, int color = 1)
+    {
+        int regionWidth = x2 - x1;
+        int regionHeight = y2 - y1;
+
+        int maxSymbolsPerLine = 0;
+        int symbolsInLine = 0;
+        int lines = 1;
+        for (int letterID = 0; letterID < strlen(letters); letterID++)
+        {
+            if ((int)letters[letterID] == '\n')
+            {
+                symbolsInLine=0;
+                lines++;
+            }
+            symbolsInLine++;
+            if(symbolsInLine>maxSymbolsPerLine){
+                maxSymbolsPerLine = symbolsInLine;
             }
         }
 
-        static void drawTextFormated(Adafruit_SSD1306 display, int x1, int y1, int x2, int y2, const uint8_t *font, char *letters, int type = 0, int color = 1){
-            int regionWidth = x2 - x1;
-            int regionHeight = y2 - y1;
+        int textWidth = pgm_read_byte(font + 0) * maxSymbolsPerLine;
+        int textHeight = pgm_read_byte(font + 1) * lines;
 
-            int textWidth = pgm_read_byte(font + 0) * strlen(letters);
-            int textHeight = pgm_read_byte(font + 1);
+        double xPos = x1;
+        double yPos = y1;
+        
 
-            double xPos = (regionWidth - textWidth) / 2 + x1;
-            double yPos = (regionHeight - textHeight) / 2 + y1;
-            
-            FontManager::drawText(display, (int)xPos, (int)yPos, font, letters, color);
+        switch (type)
+        {
+        case 1:
+            xPos = (regionWidth - textWidth) / 2 + x1;
+            yPos = (regionHeight - textHeight) / 2 + y1;
+            break;
+        
+        case 2:
+            xPos = (regionWidth - textWidth) + x1;
+            yPos = (regionHeight - textHeight) / 2 + y1;
+
+        default:
+            yPos = (regionHeight - textHeight) / 2 + y1;
+            break;
         }
+
+        FontManager::drawText(display, (int)xPos, (int)yPos, font, letters, color);
+    }
 };
